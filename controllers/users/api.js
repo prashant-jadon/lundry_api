b5261d279450281d53426e72554384c9cb6ba9fb
Prashant Jadon <bughunterprashant@gmail.com>
2025-06-20 21:46:52 +0530

    order time updated

--- controllers/users/api.js
+++ controllers/users/api.js
@@ -94,7 +94,7 @@ router.post('/login', async (req, res) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+       // sameSite: 'lax',
         maxAge: 60 * 60 * 1000 // 1 hour
     });
 
